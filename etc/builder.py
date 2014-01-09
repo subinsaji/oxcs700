4131b175624eb4db6a0d4fa5f0430ed72e92aecc
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2014-01-09 16:49:58 +0000

    OXCS700: corrected builder support

--- etc/builder.py
+++ etc/builder.py
@@ -1,7 +1,14 @@
-from iocbuilder import AutoSubstitution
+from iocbuilder import AutoSubstitution, Device
 from iocbuilder.modules.streamDevice import AutoProtocol
+from genSub import GenSub
 
-class OXPH700(AutoSubstitution, AutoProtocol):
+class OXCS700Lib(Device):
+    LibFileList=["OXCS700"]
+    DbdFileList=["OXCS700Support"]
+    AutoInstantiate=True
+
+class OXPH700(AutoSubstitution, AutoProtocol, Device):
+    Dependencies = (GenSub, OXCS700Lib)
     # Substitution attributes
     TemplateFile = 'OXPH700.template'
 
@@ -9,7 +16,8 @@ class OXPH700(AutoSubstitution, AutoProtocol):
     ProtocolFiles = ['OXCS700.proto']
 
 
-class OXCS700(AutoSubstitution, AutoProtocol):
+class OXCS700(AutoSubstitution, AutoProtocol, Device):
+    Dependencies = (GenSub, OXCS700Lib)
     # Substitution attributes
     TemplateFile = 'OXCS700.template'
 
